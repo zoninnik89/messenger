768d0d4fdb9b477804873dabc6803f10a742ed56
Nick Zonin <zoninnik@gmail.com>
2024-09-22 19:11:46 +0200

    send message and subscribe tests added

--- pub-sub/tests/mocks/mock_server_stream.go
+++ pub-sub/tests/mocks/mock_server_stream.go
@@ -2,6 +2,7 @@ package mocks
 
 import (
 	"context"
+	"fmt"
 	pb "github.com/zoninnik89/messenger/common/api"
 	"google.golang.org/grpc/metadata"
 )
@@ -9,16 +10,25 @@ import (
 // MockServerStream is a mock implementation of grpc.ServerStream
 type MockServerStream struct {
 	Messages []*pb.MessageResponse
-	Done     chan struct{}
+	DoneChan chan struct{}
+}
+
+func NewMockServerStream() *MockServerStream {
+	return &MockServerStream{
+		Messages: make([]*pb.MessageResponse, 0),
+		DoneChan: make(chan struct{}), // We control when the context is done
+	}
 }
 
 func (mss *MockServerStream) Send(msg *pb.MessageResponse) error {
+	fmt.Println("MockServerStream.Send called with message:", msg.Message)
 	mss.Messages = append(mss.Messages, msg)
 	return nil
 }
 
 // SendMsg mocks the SendMsg method in grpc.ServerStream
 func (mss *MockServerStream) SendMsg(m any) error {
+	fmt.Println("Message sent:", m)
 	mss.Messages = append(mss.Messages, m.(*pb.MessageResponse))
 	return nil
 }
@@ -30,7 +40,12 @@ func (mss *MockServerStream) RecvMsg(m any) error {
 
 // Context mocks the Context method in grpc.ServerStream
 func (mss *MockServerStream) Context() context.Context {
-	return context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		<-mss.DoneChan // Wait for the signal to cancel the context
+		cancel()
+	}()
+	return ctx
 }
 
 // SetHeader is a mock for grpc.ServerStream's SetHeader method
@@ -45,3 +60,8 @@ func (mss *MockServerStream) SendHeader(md metadata.MD) error {
 
 // SetTrailer is a mock for grpc.ServerStream's SetTrailer method
 func (mss *MockServerStream) SetTrailer(md metadata.MD) {}
+
+// Close the stream manually
+func (mss *MockServerStream) Close() {
+	close(mss.DoneChan) // Signal the stream is done (close context)
+}
--- pub-sub/tests/send_message_test.go
+++ pub-sub/tests/send_message_test.go
@@ -1,6 +1,8 @@
 package tests
 
 import (
+	"context"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	pb "github.com/zoninnik89/messenger/common/api"
 	"github.com/zoninnik89/messenger/pub-sub/service"
@@ -8,11 +10,12 @@ import (
 	"github.com/zoninnik89/messenger/pub-sub/utils"
 	"go.uber.org/zap/zaptest"
 	"testing"
+	"time"
 )
 
 func TestSubscribe_MessageSent(t *testing.T) {
 	// Create a mock stream using the manually created mock for grpc.ServerStream
-	mockStream := &mocks.MockServerStream{}
+	mockStream := mocks.NewMockServerStream()
 
 	// Create the PubSubService and test the Subscribe method
 	pubSubSvs := &service.PubSubService{
@@ -20,16 +23,31 @@ func TestSubscribe_MessageSent(t *testing.T) {
 		Logger: zaptest.NewLogger(t).Sugar(),
 	}
 
+	// Prepare the subscription request
 	req := &pb.SubscribeRequest{Chat: "test_chat"}
-	channel := make(chan *pb.MessageResponse, 1)
-	channel <- &pb.MessageResponse{Message: "test_message"}
 
-	client := &service.Client{MessageChannel: channel}
-	pubSubSvs.Chats.Add(req.Chat, client)
+	fmt.Println("Starting Subscribe goroutine")
 
-	err := pubSubSvs.Subscribe(req, mockStream)
-	assert.NoError(t, err)
+	go func() {
+		err := pubSubSvs.Subscribe(req, mockStream)
+		assert.NoError(t, err)
+	}()
 
+	time.Sleep(1 * time.Second)
+
+	// Send a message after Subscribe has started
+	fmt.Println("Sending message to channel")
+	status := pubSubSvs.Publish(context.Background(), &pb.PublishRequest{
+		Chat:    "test_chat",
+		Message: "test_message",
+	})
+	fmt.Println("Message sent to channel")
+	time.Sleep(1 * time.Second)
+	// Check if the message was sent to the mock stream
+	fmt.Println("Checking messages")
 	assert.Equal(t, 1, len(mockStream.Messages))
+	assert.Equal(t, "Message was sent", status.Status)
 	assert.Equal(t, "test_message", mockStream.Messages[0].Message)
+
+	mockStream.Close()
 }
