e583e2279d7d4d283c2b667718e45b431c9ec955
Nick Zonin <zoninnik@gmail.com>
2024-09-25 15:38:43 +0200

    sso service updates

--- sso/internal/grpc/auth/server.go
+++ sso/internal/grpc/auth/server.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"context"
 	pb "github.com/zoninnik89/messenger/common/api"
+	"github.com/zoninnik89/messenger/sso/internal/types"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -10,10 +11,11 @@ import (
 
 type serverAPI struct {
 	pb.UnimplementedAuthServiceServer
+	service types.Auth
 }
 
-func Register(s *grpc.Server) {
-	pb.RegisterAuthServiceServer(s, &serverAPI{})
+func Register(srv *grpc.Server, svs types.Auth) {
+	pb.RegisterAuthServiceServer(srv, &serverAPI{service: svs})
 }
 
 const (
@@ -21,21 +23,56 @@ const (
 )
 
 func (s *serverAPI) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
-	if req.GetEmail() == "" {
-		return nil, status.Error(codes.InvalidArgument, "email required")
+	if err := validateLoginData(req); err != nil {
+		return nil, err
 	}
-	if req.GetPassword() == "" {
-		return nil, status.Error(codes.InvalidArgument, "password required")
-	}
-	if req.GetAppId() == emptyValue {
-		return nil, status.Error(codes.InvalidArgument, "app id required")
+
+	token, err := s.service.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal server error")
 	}
 
 	return &pb.LoginResponse{
-		Token: "",
+		Token: token,
 	}, nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
-	panic("implement me")
+	if err := validateRegisterData(req); err != nil {
+		return nil, err
+	}
+
+	userID, err := s.service.Register(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
+	return &pb.RegisterResponse{
+		UserId: userID,
+	}, nil
+}
+
+func validateLoginData(req *pb.LoginRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email required")
+	}
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password required")
+	}
+	if req.GetAppId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "app id required")
+	}
+
+	return nil
+}
+
+func validateRegisterData(req *pb.RegisterRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email required")
+	}
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password required")
+	}
+
+	return nil
 }
---
+++ sso/internal/types/types.go
@@ -0,0 +1,10 @@
+package types
+
+import (
+	"context"
+)
+
+type Auth interface {
+	Login(ctx context.Context, email string, password string, appID int) (token string, err error)
+	Register(ctx context.Context, email string, password string) (userID int64, err error)
+}
