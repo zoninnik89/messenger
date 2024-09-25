b35d0629b96801ccaba3fb509f2769d74ebc7a3a
Nick Zonin <zoninnik@gmail.com>
2024-09-25 17:58:43 +0200

    sso service updates

--- sso/internal/services/auth/auth.go
+++ sso/internal/services/auth/auth.go
@@ -1,1 +1,9 @@
 package auth
+
+import "go.uber.org/zap"
+
+type Auth struct {
+	logger *zap.SugaredLogger
+}
+
+func NewAuthService(logger *zap.SugaredLogger) *Auth {}
--- sso/internal/types/types.go
+++ sso/internal/types/types.go
@@ -2,9 +2,16 @@ package types
 
 import (
 	"context"
+	"github.com/zoninnik89/messenger/sso/internal/domain/models"
 )
 
 type Auth interface {
 	Login(ctx context.Context, email string, password string, appID int) (token string, err error)
 	Register(ctx context.Context, email string, password string) (userID int64, err error)
+}
+
+type Storage interface {
+	SaveUser(ctx context.Context) (uid int64, err error)
+	UpdateUser(ctx context.Context, user models.User) error
+	GetUser(ctx context.Context, email string) (models.User, error)
 }
